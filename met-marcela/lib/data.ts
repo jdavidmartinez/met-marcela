5d93fd7972b3658596fafaf9bf4cd5fd26bb1b43
DaviD <jdavid.martinez@gmail.com>
2026-06-04 14:30:50 -0500

    fix: export questions from data.ts

---
+++ met-marcela/lib/data.ts
@@ -0,0 +1,11 @@
+// lib/data.ts
+
+export const questions = [
+    {
+      id: "1",
+      questionText: "What is the capital of Michigan?",
+      options: ["Detroit", "Lansing", "Ann Arbor", "Grand Rapids"],
+      correctAnswer: "Lansing",
+    },
+    // Add more questions here...
+  ];
